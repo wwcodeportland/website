50ec73edd89b113c9dc2645f37b3b52c0e287ec1
Gracie Lundell <gracielundell@gmail.com>
2019-01-05 23:00:33 -0800

    add parallax functionality to spacer

--- js/scripts.js
+++ js/scripts.js
@@ -2,3 +2,9 @@ document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.collapsible');
     var instances = M.Collapsible.init(elems);
   });
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    var spacer = document.querySelectorAll('.parallax');
+    var instances = M.Parallax.init(spacer);
+  });
